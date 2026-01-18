496cd2e060e53829c833df5bc5695568b6bbd18a
davidm603 <d_mcmurray@live.com>
2026-01-18 17:22:54 -0600

    day 2 part 1

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ Solutions to 2025's Advent of Code puzzles as I work through them fashionably la
     <summary>Day 1 Part 2</summary>
     The easiest way to count how many times zero is passed would certainly be to do the ticks one at a time, but I'm still interested in avoiding that. Counting each full turn in a rotation (e.g. there are 2 full turns in L203), plus each time the dial position variable crosses out of bounds when adding the remainder (e.g. L203 -> handle the 2 turns -> -3 remainder, could cross zero if dial's at 1), plus each time it lands on zero as in part 1, should handle everything. The tricky part was avoiding an off-by-one when starting from zero and rotating left.
 </details>
+
+<details>
+    <summary>Day 2 Part 1</summary>
+    I decided to loop through the ranges and check by brute force. Hypothetically, I could trade space to save time by pre-generating all the numbers that consist of repeating the digits of smaller numbers, up to the maximum top-end of any range, but it seems unnecessary for the inputs given.
+</details>
+
+<details>
+    <summary>Day 2 Part 2</summary>
+    todo!
+</details>
--- rust/src/bin/2.rs
+++ rust/src/bin/2.rs
@@ -1,11 +1,16 @@
+use std::{fmt::Write, io::BufRead};
 fn main() {
     let puzzle_text = advent_of_code_2025::load_puzzle_string(2);
 
     let mut p1_invalid_count = 0;
-    for (low, high) in parse_d2_ranges(&puzzle_text) {}
+    for (low, high) in parse_d2_ranges(&puzzle_text) {
+        p1_invalid_count += sum_invalid_p1_ids_in_range(low, high)
+    }
+    dbg!(p1_invalid_count);
 }
 fn parse_d2_ranges(puzzle: &str) -> impl Iterator<Item = (u64, u64)> {
     puzzle
+        .trim()
         .split(',')
         .filter(|comma_sep| !comma_sep.is_empty())
         .map(|text| {
@@ -14,4 +19,45 @@ fn parse_d2_ranges(puzzle: &str) -> impl Iterator<Item = (u64, u64)> {
             let high = high_t.parse().unwrap();
             (low, high)
         })
+}
+/// Check if the number is some sequence of digits repeated twice.
+fn id_is_invalid_p1(num: u64, buffer: &mut String) -> bool {
+    // There are certainly ways to do this without touching memory, with ilog...
+    // but converting the number to a string makes things simple.
+    // I'll reuse a string instead of creating new ones, to keep performance reasonable.
+    buffer.clear();
+    _ = write!(buffer, "{num}");
+    let half_len = buffer.len() / 2;
+    let even_len = buffer.len() == half_len * 2;
+    if !even_len {
+        return false;
+    }
+    let (first_half, second_half) = buffer.as_bytes().split_at(half_len);
+    first_half == second_half
+}
+fn sum_invalid_p1_ids_in_range(start: u64, end: u64) -> u64 {
+    let mut reusable_string = String::new();
+    (start..=end)
+        .map(|n| {
+            id_is_invalid_p1(n, &mut reusable_string)
+                .then_some(n)
+                .unwrap_or_default()
+        })
+        .sum()
+}
+/// If the id is a repetition of digits, of any length, it's invalid and should return true. Otherwise false.
+fn id_is_invalid_p2(num: u64, buffer: &mut String) -> bool {
+    buffer.clear();
+    _ = write!(buffer, "{num}");
+    false
+}
+fn sum_invalid_p2_ids_in_range(start: u64, end: u64) -> u64 {
+    let mut reusable_string = String::new();
+    (start..=end)
+        .map(|n| {
+            id_is_invalid_p2(n, &mut reusable_string)
+                .then_some(n)
+                .unwrap_or_default()
+        })
+        .sum()
 }
